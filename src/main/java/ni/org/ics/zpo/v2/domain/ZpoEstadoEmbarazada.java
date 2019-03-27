8eed97dc7d35afb0599a83d7cba5c8ee068630b0
Miguel Salinas <msalinas@icsnicaragua.org>
2019-03-27 12:31:09 -0600

    Formulario resultado evaluación oftalmológica

--- src/main/java/ni/org/ics/zpo/v2/domain/ZpoEstadoEmbarazada.java
+++ src/main/java/ni/org/ics/zpo/v2/domain/ZpoEstadoEmbarazada.java
@@ -21,8 +21,17 @@ public class ZpoEstadoEmbarazada extends BaseMetaData implements Auditable{
 	private static final long serialVersionUID = 1L;
 	private String recordId;
 	private char ingreso = '0';
-	private char mes12 = '0';
 	private char mes24 = '0';
+	private char mes30 = '0';
+    private char mes36 = '0';
+    private char mes42 = '0';
+    private char mes48 = '0';
+    private char mes54 = '0';
+    private char mes60 = '0';
+    private char mes66 = '0';
+    private char mes72 = '0';
+    private char mes78 = '0';
+    private char mes84 = '0';
 	
 	@Id
     @Column(name = "record_id", nullable = false, length = 25)
@@ -43,25 +52,106 @@ public class ZpoEstadoEmbarazada extends BaseMetaData implements Auditable{
 		this.ingreso = ingreso;
 	}
 
-    @Column(name="mes12", nullable = false, length = 1)
-    public char getMes12() {
-        return mes12;
-    }
-
-    public void setMes12(char mes12) {
-        this.mes12 = mes12;
-    }
-
     @Column(name="mes24", nullable = false, length = 1)
-	public char getMes24() {
-		return mes24;
+    public char getMes24() {
+        return mes24;
+    }
+
+    public void setMes24(char mes12) {
+        this.mes24 = mes12;
+    }
+
+    @Column(name="mes30", nullable = false, length = 1)
+	public char getMes30() {
+		return mes30;
 	}
 
-	public void setMes24(char mes24) {
-		this.mes24 = mes24;
+	public void setMes30(char mes24) {
+		this.mes30 = mes24;
 	}
 
-	@Override
+    @Column(name="mes36", nullable = false, length = 1)
+    public char getMes36() {
+        return mes36;
+    }
+
+    public void setMes36(char mes36) {
+        this.mes36 = mes36;
+    }
+
+    @Column(name="mes42", nullable = false, length = 1)
+    public char getMes42() {
+        return mes42;
+    }
+
+    public void setMes42(char mes42) {
+        this.mes42 = mes42;
+    }
+
+    @Column(name="mes48", nullable = false, length = 1)
+    public char getMes48() {
+        return mes48;
+    }
+
+    public void setMes48(char mes48) {
+        this.mes48 = mes48;
+    }
+
+    @Column(name="mes54", nullable = false, length = 1)
+    public char getMes54() {
+        return mes54;
+    }
+
+    public void setMes54(char mes54) {
+        this.mes54 = mes54;
+    }
+
+    @Column(name="mes60", nullable = false, length = 1)
+    public char getMes60() {
+        return mes60;
+    }
+
+    public void setMes60(char mes60) {
+        this.mes60 = mes60;
+    }
+
+    @Column(name="mes66", nullable = false, length = 1)
+    public char getMes66() {
+        return mes66;
+    }
+
+    public void setMes66(char mes66) {
+        this.mes66 = mes66;
+    }
+
+    @Column(name="mes72", nullable = false, length = 1)
+    public char getMes72() {
+        return mes72;
+    }
+
+    public void setMes72(char mes72) {
+        this.mes72 = mes72;
+    }
+
+    @Column(name="mes78", nullable = false, length = 1)
+    public char getMes78() {
+        return mes78;
+    }
+
+    public void setMes78(char mes78) {
+        this.mes78 = mes78;
+    }
+
+    @Column(name="mes84", nullable = false, length = 1)
+    public char getMes84() {
+        return mes84;
+    }
+
+    public void setMes84(char mes84) {
+        this.mes84 = mes84;
+    }
+
+    @Override
 	public boolean isFieldAuditable(String fieldname) {
 		return true;
 	}
